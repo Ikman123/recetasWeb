4fd1262501d69500cf178f981cfc2fc862750c9b
ikman123 <88504533+Ikman123@users.noreply.github.com>
2023-08-28 22:13:11 -0300

    tercer commit

--- recetasnacho6to/src/components/Card.js
+++ recetasnacho6to/src/components/Card.js
@@ -6,7 +6,6 @@ function Card(props) {
         <div className='card-container'>
             <div className='image-container'>
                 <img src={props.imgUrl} alt='' />
-                <img src={props.iconUrl} alt='' />
             </div>
             <div className='card-content'>
                 <div className='card-title'>
@@ -15,6 +14,19 @@ function Card(props) {
                 <div className='card-descripcion'>
                     <p>{props.descripcion}</p>
                 </div>
+            </div>
+            <div className='card-icons'>
+                <h4>Ingredientes</h4>
+                <div className='card-icon'>
+                    <img src={props.icon1} alt='' />
+                    <img src={props.icon2} alt='' />
+                    <img src={props.icon3} alt='' />
+                    <img src={props.icon4} alt='' />
+                    <img src={props.icon5} alt='' />
+                    <img src={props.icon6} alt='' />
+                    <img src={props.icon7} alt='' />
+                </div>
+                
             </div>
         </div>
     );
